e22f4209080b58220f63cb2e15645c81d1e9bdb4
Mathias Rav <rav@cs.au.dk>
2017-05-16 21:10:10 -0400

    Make testsmtpd work with requests

--- testsmtpd/__main__.py
+++ testsmtpd/__main__.py
@@ -1,15 +1,23 @@
+import io
 import aiohttp
 import argparse
+import requests
 import aiosmtpd.controller
 
 
 class Handler:
     def __init__(self, args):
-        self.url = 'http://127.0.0.1:%s/api/submit/' % args.http_port
+        try:
+            int(args.http_port)
+        except ValueError:
+            self.url = '%s/api/submit/' % args.http_port
+        else:
+            self.url = 'http://127.0.0.1:%s/api/submit/' % args.http_port
         self.key = args.key
 
     async def handle_DATA(self, server, session, envelope):
-        async with aiohttp.ClientSession(loop=server.loop) as session:
+        # async with aiohttp.ClientSession(loop=server.loop) as session:
+        with requests.Session() as session:
             for rcpt in envelope.rcpt_tos:
                 try:
                     print("From %r to %r..." % (envelope.mail_from, rcpt), end='')
@@ -17,14 +25,19 @@ class Handler:
                         key=self.key,
                         mail_from=envelope.mail_from,
                         rcpt_to=rcpt,
-                        message_bytes=envelope.content,
                     )
-                    async with session.post(self.url, data=data) as response:
-                        text = await response.text()
-                        status = response.status
+                    files = dict(
+                        message_bytes=('message.msg', io.BytesIO(envelope.content)),
+                    )
+                    response = session.post(self.url, data=data, files=files)
+                    text = response.text
+                    status = response.status_code
+                    # async with session.post(self.url, data=data) as response:
+                    #     text = await response.text()
+                    #     status = response.status
                     if status != 200 or text.strip() != '250 OK':
                         print(' HTTP %s %r' %
-                              (status, text.splitlines()[0][:50]), end='')
+                              (status, text.splitlines()[0][:200]), end='')
                         return '500 Internal server error'
                     print(' OK', end='')
                 finally:
@@ -35,7 +48,7 @@ class Handler:
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-P', '--smtp-port', type=int, default=1025)
-    parser.add_argument('-p', '--http-port', type=int, default=8000)
+    parser.add_argument('-p', '--http-port', default=8000)
     parser.add_argument('-k', '--key', required=True)
     args = parser.parse_args()
 
