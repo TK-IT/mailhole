aaa085aaed0ff90c7add2cca5690ef7fe3e5fc72
Mathias Rav <rav@cs.au.dk>
2017-05-17 19:48:52 -0400

    Add MEDIA_URL to settings

--- mailhole/settings/common.py
+++ mailhole/settings/common.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+MEDIA_URL = '/uploads/'
 LOGIN_URL = '/login/'
 
 SITE_NAME = 'mail.tket.dk'
