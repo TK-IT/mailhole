a2d387dadd1882e5d015474b5de345cac6a1758d
Mathias Rav <rav@cs.au.dk>
2017-07-21 13:37:34 +0200

    Add production settings

---
+++ mailhole/settings/prod.py
@@ -0,0 +1,68 @@
+from .common import *
+
+ADMINS = (
+    ('Mathias Rav', 'rav@cs.au.dk'),
+)
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+DEBUG = False
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'prodekanus/static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'prodekanus/uploads')
+
+ALLOWED_HOSTS = ['mail.tket.dk']
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+if DATABASES['default']['ENGINE'] == 'django.db.backends.mysql':
+    DATABASES['default'].setdefault('OPTIONS', {})['init_command'] = (
+        "SET sql_mode='STRICT_TRANS_TABLES'")
+
+EMAIL_HOST = 'smtp01.uni.au.dk'
+SERVER_EMAIL = 'mailhole@prodekanus.studorg.au.dk'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'django-email')
+
+# EMAIL_HOST = 'smtp-clients.au.dk'
+# EMAIL_HOST_USER = 'au306325@uni.au.dk'
+# from base64 import b64decode as _b64decode
+# EMAIL_HOST_PASSWORD = _b64decode(...)
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = False
+# EMAIL_USE_SSL = False
+# EMAIL_TIMEOUT = 5
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': ('[%(asctime)s %(name)s %(levelname)s] ' +
+                       '%(message)s'),
+        },
+    },
+    'handlers': {
+        'file': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': os.path.join(BASE_DIR, 'prodekanus/django.log'),
+            'formatter': 'simple',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file', 'mail_admins'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'WARNING'),
+        },
+        'mailhole': {
+            'handlers': ['file', 'mail_admins'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
